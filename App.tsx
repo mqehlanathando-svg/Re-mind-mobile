3d811d6f53a24dfbf790db512fc3cbd71f2af4c5
mqehlanathando-svg <mqehlanathando@gmail.com>
2026-02-04 12:41:26 +0000

    info-fix

--- App.tsx
+++ App.tsx
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <SafeAreaView style={styles.container}>
       <WebView
-        source={{ uri: "https://YOUR-WEB-APP-URL" }}
+        source={{ uri: "https://7d789156-4b66-4ac2-9fc9-e1157727b0f5-00-368pk1s71uwm7.riker.replit.dev/" }}
         javaScriptEnabled
         domStorageEnabled
       />
